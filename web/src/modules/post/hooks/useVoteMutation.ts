ef4c9493c22ab70ec39b0321b58cc6701f7ec70f
edwces <68016750+EdwinC0@users.noreply.github.com>
2022-06-16 10:00:13 +0200

    fix query invalidation

--- web/src/modules/post/hooks/useVoteMutation.ts
+++ web/src/modules/post/hooks/useVoteMutation.ts
@@ -10,22 +10,22 @@ export function useVoteMutation() {
     ({ id, type }: { id: number; type: PostVoteType }) => votePost(id, type),
     {
       onSuccess: (updatedPost, { type }) => {
-        console.log(updatedPost);
-
-        queryClient.setQueryData<InfiniteData<Post[]>>("posts", (old) => {
-          const newData = {
-            pages: old?.pages.map((page) => {
-              return page.map((post) =>
-                post.id == updatedPost!.post.id
-                  ? { ...updatedPost.post, voteStatus: type }
-                  : post
-              );
-            }),
-            pageParams: old?.pageParams,
-          } as InfiniteData<Post[]>;
-          console.log(newData);
-          return newData;
-        });
+        queryClient.setQueryData<InfiniteData<Post[]>>(
+          ["posts", { order: "desc", sort: "likes" }],
+          (old) => {
+            const newData = {
+              pages: old?.pages.map((page) => {
+                return page.map((post) =>
+                  post.id == updatedPost!.post.id
+                    ? { ...updatedPost.post, voteStatus: type }
+                    : post
+                );
+              }),
+              pageParams: old?.pageParams,
+            } as InfiniteData<Post[]>;
+            return newData;
+          }
+        );
       },
     }
   );
